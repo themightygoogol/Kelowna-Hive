84287b8a19fa03b2d4f4d8080bdfd3fba6f010fb
rhythm5099 <rhythmkataria60@gmail.com>
2025-04-06 16:55:05 -0700

    Category Page

---
+++ main/java/com/example/kelownahiveapp/category/CategoryAdapter.java
@@ -0,0 +1,56 @@
+package com.example.kelownahiveapp.category;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.kelownahiveapp.R;
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
+    private final List<CategoryItem> categories;
+    private final OnCategoryClickListener listener;
+
+    public interface OnCategoryClickListener {
+        void onCategoryClick(String categoryName);
+    }
+
+    public CategoryAdapter(List<CategoryItem> categories, OnCategoryClickListener listener) {
+        this.categories = categories;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_item, parent, false);
+        return new CategoryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
+        CategoryItem category = categories.get(position);
+        holder.name.setText(category.getName());
+        holder.icon.setImageResource(category.getIconResId());
+        holder.itemView.setOnClickListener(v -> listener.onCategoryClick(category.getName()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.size();
+    }
+
+    static class CategoryViewHolder extends RecyclerView.ViewHolder {
+        TextView name;
+        ImageView icon;
+
+        public CategoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.categoryName);
+            icon = itemView.findViewById(R.id.categoryIcon);
+        }
+    }
+}
---
+++ main/java/com/example/kelownahiveapp/category/CategoryItem.java
@@ -0,0 +1,19 @@
+package com.example.kelownahiveapp.category;
+
+public class CategoryItem {
+    private final String name;
+    private final int iconResId;
+
+    public CategoryItem(String name, int iconResId) {
+        this.name = name;
+        this.iconResId = iconResId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getIconResId() {
+        return iconResId;
+    }
+}
